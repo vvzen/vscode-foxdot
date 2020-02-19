420384fdf82bc3af1504793e118a3bd388f096cb
Valerio Viperino <10340139+vvzen@users.noreply.github.com>
2020-02-19 22:07:15 +0000

    Add first keybinding for executing the current selection (cmd+enter). It works!!

--- foxdot.js
+++ foxdot.js
@@ -4,6 +4,8 @@ const child_process = require('child_process');
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
+const editor = vscode.window.activeTextEditor;
+
 let foxDotProcess;
 
 // this method is called when your extension is activated
@@ -12,7 +14,6 @@ let foxDotProcess;
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('activating "vscode-foxdot"..');
@@ -20,10 +21,15 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('foxdot.start', start);
+	let disposables = [];
+	disposables.push(vscode.commands.registerCommand('foxdot.start', start));
+	disposables.push(vscode.commands.registerCommand('foxdot.evaluateSelection', evaluateSelection));
 
-	context.subscriptions.push(disposable);
+	disposables.forEach((disposable) => {
+		context.subscriptions.push(disposable);
+	});
 }
+
 exports.activate = activate;
 
 
@@ -61,6 +67,21 @@ function deactivate() {
 	console.log(`killing FoxDot..`);
 	foxDotProcess.kill("SIGINT");
 	foxDotProcess = null;
+}
+
+function evaluateSelection(){
+	const selectedText = editor.document.getText(editor.selection);
+	evaluateCode(selectedText);
+}
+
+/**
+ * @param {string} code
+ */
+function evaluateCode(code) {
+	console.log(`evaluating: ${code}..`);
+
+	foxDotProcess.stdin.write(code);
+	foxDotProcess.stdin.write('\n\n');
 }
 
 module.exports = {
